cb5087b431f7e824969adbd8ecedb104eee54f3c
Thet Naing Tun <thetnaingtun@bindez.com>
2021-08-16 00:35:32 +0630

    move style from scss to styled component in app-form

--- src/components/form/index.js
+++ src/components/form/index.js
@@ -1,83 +1,127 @@
-import React, { useContext, useState } from 'react';
+import React, { useContext, useState } from "react";
+import styled from "styled-components/macro";
 
 import { AppModal, AppExplain } from "../index";
-import AppCodeContainer from '../../container/code';
+import AppCodeContainer from "../../container/code";
 
 import GridContext from "../../context/gridContext";
 import FormContext from "../../context/formContext";
 
-import '../../assets/styles/app-form.scss';
+const StyledWat = styled.p`
+  font-family: "Caveat", cursive;
+  font-size: 22px;
+  margin-top: 18px;
+  color: #a8c9cc;
+  opacity: 0.9;
+  font-weight: normal;
+  cursor: pointer;
+`;
+
+const StyledControlPanel = styled.aside`
+  margin: 60px 60px;
+  font-size: 17px;
+  width: 300px;
+  button[type="reset"] {
+    @media screen and (max-width: 700px) {
+      width: 80vw;
+      margin: 100px 50px;
+    }
+  }
+  @media screen and (max-width: 700px) {
+    width: 80vw;
+    margin: 100px 50px;
+  }
+`;
 
 export default function AppForm() {
+  let {
+    columns,
+    setColumns,
+    rows,
+    setRows,
+    rowGap,
+    setRowGap,
+    columnGap,
+    setColumnGap,
+  } = useContext(FormContext);
+  let { setChildArea } = useContext(GridContext);
 
-	let { columns, setColumns, rows, setRows, rowGap, setRowGap, columnGap, setColumnGap } = useContext(FormContext);
-	let { setChildArea } = useContext(GridContext);
-
-	let [explainModalStatus, setExplainModalStatus] = useState(false);
-	let [codeModalStatus, setCodeModalStatus] = useState(false);
-	return (
-		<aside>
-			<fieldset>
-				<label htmlFor="columns">Columns</label>
-				<input
-					id="columns"
-					type="number"
-					min={0}
-					max={12}
-					value={columns}
-					onChange={(event) => setColumns(event.target.value)} />
-			</fieldset>
-			<fieldset>
-				<label htmlFor="rows">Rows</label>
-				<input
-					id="rows"
-					type="number"
-					min={0}
-					max={12}
-					value={rows}
-					onChange={(event) => setRows(event.target.value)} />
-			</fieldset>
-			<fieldset>
-				<label htmlFor="column-gap">Column Gap</label>
-				<input
-					id="column-gap"
-					type="number"
-					min={0}
-					max={50}
-					value={columnGap}
-					onChange={(event) => setColumnGap(event.target.value)}
-				/>
-			</fieldset>
-			<fieldset>
-				<label htmlFor="row-gap">Row Gap</label>
-				<input
-					id="row-gap"
-					type="number"
-					min={0}
-					max={50}
-					value={rowGap}
-					onChange={(event) => setRowGap(event.target.value)}
-				/>
-			</fieldset>
-			<button onClick={() => setCodeModalStatus(true)}>Please may I have some code</button>
-			<button type="reset" onClick={() => setChildArea([])}>Reset Grid</button>
-			<AppModal modalHandler={setCodeModalStatus} modalStatus={codeModalStatus}>
-				<AppModal.Header>
-					<h3>Your Grid Code</h3>
-				</AppModal.Header>
-				<AppModal.Section>
-					<AppCodeContainer />
-				</AppModal.Section>
-			</AppModal>
-			<p className="wat" onClick={() => setExplainModalStatus(true)}>What does this project do?</p>
-			<AppModal modalHandler={setExplainModalStatus} modalStatus={explainModalStatus}>
-				<AppModal.Header>
-					<h3>What is this?</h3>
-				</AppModal.Header>
-				<AppModal.Section>
-					<AppExplain />
-				</AppModal.Section>
-			</AppModal>
-		</aside>
-	)
+  let [explainModalStatus, setExplainModalStatus] = useState(false);
+  let [codeModalStatus, setCodeModalStatus] = useState(false);
+  return (
+    <StyledControlPanel>
+      <fieldset>
+        <label htmlFor="columns">Columns</label>
+        <input
+          id="columns"
+          type="number"
+          min={0}
+          max={12}
+          value={columns}
+          onChange={(event) => setColumns(event.target.value)}
+        />
+      </fieldset>
+      <fieldset>
+        <label htmlFor="rows">Rows</label>
+        <input
+          id="rows"
+          type="number"
+          min={0}
+          max={12}
+          value={rows}
+          onChange={(event) => setRows(event.target.value)}
+        />
+      </fieldset>
+      <fieldset>
+        <label htmlFor="column-gap">Column Gap</label>
+        <input
+          id="column-gap"
+          type="number"
+          min={0}
+          max={50}
+          value={columnGap}
+          onChange={(event) => setColumnGap(event.target.value)}
+        />
+      </fieldset>
+      <fieldset>
+        <label htmlFor="row-gap">Row Gap</label>
+        <input
+          id="row-gap"
+          type="number"
+          min={0}
+          max={50}
+          value={rowGap}
+          onChange={(event) => setRowGap(event.target.value)}
+        />
+      </fieldset>
+      <button onClick={() => setCodeModalStatus(true)}>
+        Please may I have some code
+      </button>
+      <button type="reset" onClick={() => setChildArea([])}>
+        Reset Grid
+      </button>
+      <AppModal modalHandler={setCodeModalStatus} modalStatus={codeModalStatus}>
+        <AppModal.Header>
+          <h3>Your Grid Code</h3>
+        </AppModal.Header>
+        <AppModal.Section>
+          <AppCodeContainer />
+        </AppModal.Section>
+      </AppModal>
+      <StyledWat onClick={() => setExplainModalStatus(true)}>
+        What does this project do?
+      </StyledWat>
+      <AppModal
+        modalHandler={setExplainModalStatus}
+        modalStatus={explainModalStatus}
+      >
+        <AppModal.Header>
+          <h3>What is this?</h3>
+        </AppModal.Header>
+        <AppModal.Section>
+          <AppExplain />
+        </AppModal.Section>
+      </AppModal>
+    </StyledControlPanel>
+  );
 }
