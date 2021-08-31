f2d8fdc0ca6b720c8bd74c6e2c60377334dccd55
Thet Naing Tun <thetnaingtun@bindez.com>
2021-08-31 17:25:01 +0630

    move some scss style to style-component in AppGridColumnSizeSelector

--- src/components/grid/styles.js
+++ src/components/grid/styles.js
@@ -1,38 +1,43 @@
-import styled from 'styled-components';
+import styled, { css } from "styled-components/macro";
 
 export const AppGridColumnSizeSelector = styled.section`
-    grid-template-columns: ${({colTemplate}) => colTemplate};
-    grid-template-rows: 50px;
-    column-gap:${({columnGap}) => `${columnGap}px`};
-    row-gap:${({rowGap}) => `${rowGap}px`};
+  display: grid;
+  grid-template-columns: ${({ colTemplate }) => colTemplate};
+  grid-template-rows: 50px;
+  column-gap: ${({ columnGap }) => `${columnGap}px`};
+  row-gap: ${({ rowGap }) => `${rowGap}px`};
+  div {
+    text-align: center;
+    position: relative;
+  }
+  ${({ widthfull }) =>
+    widthfull &&
+    css`
+      width: 100%;
+    `}
 `;
 
 export const AppGridRowSizeSelector = styled.section`
-    grid-template-columns: 50px;
-    grid-template-rows: ${({rowTemplate}) => rowTemplate};
-    column-gap:${({columnGap}) => `${columnGap}px`};
-    row-gap:${({rowGap}) => `${rowGap}px`};
+  grid-template-columns: 50px;
+  grid-template-rows: ${({ rowTemplate }) => rowTemplate};
+  column-gap: ${({ columnGap }) => `${columnGap}px`};
+  row-gap: ${({ rowGap }) => `${rowGap}px`};
 `;
 
 export const Grid = styled.section`
-	grid-template-columns: ${({colTemplate}) => colTemplate};
-    grid-template-rows: ${({rowTemplate}) => rowTemplate};
-    column-gap:${({columnGap}) => `${columnGap}px`};
-    row-gap:${({rowGap}) => `${rowGap}px`};
+  grid-template-columns: ${({ colTemplate }) => colTemplate};
+  grid-template-rows: ${({ rowTemplate }) => rowTemplate};
+  column-gap: ${({ columnGap }) => `${columnGap}px`};
+  row-gap: ${({ rowGap }) => `${rowGap}px`};
 `;
 
 export const GridChildrenArea = styled.section`
-	grid-template-columns: ${({colTemplate}) => colTemplate};
-    grid-template-rows: ${({rowTemplate}) => rowTemplate};
-    column-gap:${({columnGap}) => `${columnGap}px`};
-    row-gap:${({rowGap}) => `${rowGap}px`};
+  grid-template-columns: ${({ colTemplate }) => colTemplate};
+  grid-template-rows: ${({ rowTemplate }) => rowTemplate};
+  column-gap: ${({ columnGap }) => `${columnGap}px`};
+  row-gap: ${({ rowGap }) => `${rowGap}px`};
 `;
 
 export const GridChildren = styled.div`
-	grid-area: ${({childArea}) => childArea}
-`
-
-
-
-
-
+  grid-area: ${({ childArea }) => childArea};
+`;
