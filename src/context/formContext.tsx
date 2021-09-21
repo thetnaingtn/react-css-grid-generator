879387b54cede3c3d221bb2fc79d73486997e6af
Thet Naing Tun <thetnaingtun@bindez.com>
2021-09-22 00:23:54 +0630

    form context types

--- src/context/formContext.tsx
+++ src/context/formContext.tsx
@@ -1,18 +1,37 @@
-import React, {createContext, useState} from 'react';
+import React, { createContext, useState } from "react";
 
-const FormContext = createContext();
+type FormContextTypes = {
+  columns: number;
+  setColumns: (val: number) => void;
+  rows: number;
+  setRows: (val: number) => void;
+  columnGap: number;
+  setColumnGap: (val: number) => void;
+  rowGap: number;
+  setRowGap: (val: number) => void;
+};
+
+const FormContext = createContext<FormContextTypes>({} as FormContextTypes);
+
+export function FormProvider({ children }: React.PropsWithChildren<{}>) {
+  let [columns, setColumns] = useState(5);
+  let [rows, setRows] = useState(5);
+  let [columnGap, setColumnGap] = useState(0);
+  let [rowGap, setRowGap] = useState(0);
+
+  let value = {
+    columns,
+    setColumns,
+    rows,
+    setRows,
+    columnGap,
+    setColumnGap,
+    rowGap,
+    setRowGap,
+  };
+  return (
+    <FormContext.Provider value={{ ...value }}>{children}</FormContext.Provider>
+  );
+}
 
 export default FormContext;
-export function FormProvider({children}){
-	let [columns, setColumns] = useState(5);
-	let [rows, setRows] = useState(5);
-	let [columnGap, setColumnGap] = useState(0);
-	let [rowGap, setRowGap] = useState(0);
-
-	let value = {columns, setColumns, rows, setRows, columnGap, setColumnGap, rowGap, setRowGap};
-	return(
-		<FormContext.Provider value={{...value}}>
-			{children}
-		</FormContext.Provider>
-	)
-}
