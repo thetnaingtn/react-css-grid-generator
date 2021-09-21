e9c29fe62c75bdbb511b7365aecaf45f301c9cdb
Thet Naing Tun <thetnaingtun@bindez.com>
2021-09-21 23:48:49 +0630

    move all style to styled-components and remove the whole scss

--- src/components/explain/index.js
+++ src/components/explain/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 import styled from "styled-components";
 import { ExplainStyles } from "./styles";
-import "../../assets/styles/app-explain.scss";
 
 const StylesCursive = styled.p`
   font-family: "Caveat", cursive;
